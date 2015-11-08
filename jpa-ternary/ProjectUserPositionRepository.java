a009f82b2318a0ab57aa2f58fe45be191e260952
David Lizárraga <dlizarra5@gmail.com>
2015-11-08 18:35:21 +0100

    Added repository class

---
+++ jpa-ternary/ProjectUserPositionRepository.java
@@ -0,0 +1,7 @@
+package com.dlizarra.startuphub.project;
+
+import com.dlizarra.startuphub.support.jpa.CustomCrudRepository;
+
+public interface ProjectUserPositionRepository extends CustomCrudRepository<ProjectUserPosition, Integer> {
+
+}
